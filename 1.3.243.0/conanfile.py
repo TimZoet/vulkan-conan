4cabb98a35bd215c0b591eab977c207ab7c0cd1a
TimZoet <ts.zoet@gmail.com>
2023-05-05 16:00:52 +0200

    Added conanfile for 1.3.243.0.

---
+++ 1.3.243.0/conanfile.py
@@ -0,0 +1,31 @@
+from conan import ConanFile
+from conan.tools.files import copy
+
+class VulkanConan(ConanFile):
+    name = "vulkan"
+    version = "1.3.243.0"
+    description = "..."
+    settings = ("os", "compiler", "build_type", "arch")
+
+    def export_sources(self):
+        copy(self, "Lib/shaderc_combined.lib", self.recipe_folder, self.export_sources_folder)
+        copy(self, "Lib/shaderc_combinedd.lib", self.recipe_folder, self.export_sources_folder)
+        copy(self, "Lib/vulkan-1.lib", self.recipe_folder, self.export_sources_folder)
+        copy(self, "Include/vk_video/*", self.recipe_folder, self.export_sources_folder)
+        copy(self, "Include/vma/*", self.recipe_folder, self.export_sources_folder)
+        copy(self, "Include/vulkan/*", self.recipe_folder, self.export_sources_folder)
+        copy(self, "Include/shaderc/*", self.recipe_folder, self.export_sources_folder)
+
+    def package_info(self):
+        self.cpp_info.components["core"].libs = ["vulkan-1"]
+        if self.settings.build_type == "Debug":
+            self.cpp_info.components["shaderc"].libs = ["shaderc_combinedd"]
+        else:
+            self.cpp_info.components["shaderc"].libs = ["shaderc_combined"]
+
+    def package(self):
+        copy(self, "Lib/*", self.export_sources_folder, self.package_folder)
+        copy(self, "Include/vk_video/*", self.export_sources_folder, self.package_folder)
+        copy(self, "Include/vma/*", self.export_sources_folder, self.package_folder)
+        copy(self, "Include/vulkan/*", self.export_sources_folder, self.package_folder)
+        copy(self, "Include/shaderc/*", self.export_sources_folder, self.package_folder)
